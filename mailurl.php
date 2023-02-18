9734a2825e0e6f3ef0d74cde721a0a07903eb014
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-02-19 01:51:50 +0300

    Update plugin metadata

--- mailurl.php
+++ mailurl.php
@@ -2,15 +2,13 @@
 
 /**
  * Plugin Name: Mail URL
- * Plugin URI:  https://github.com/voronkovich
+ * Plugin URI:  https://github.com/voronkovich/wordpress-mail-url
  * Description: Configure wp_mail() via MAIL_URL environment variable
  * Version:     0.0.1
  * License:     MIT
  * License URI: https://github.com/voronkovich/wordpress-mail-url/blob/main/LICENSE
  * Author:      Oleg Voronkovich <oleg-voronkovich@yandex.ru>
  * Author URI:  https://github.com/voronkovich
- * Text Domain: mailurl
- * Domain Path: /languages
  */
 
 defined('ABSPATH') || exit;
