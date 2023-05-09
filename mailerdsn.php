5f013643e058cf72d016b003ff4958668bf5cab3
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-05-09 20:42:56 +0300

    Bump version

--- mailerdsn.php
+++ mailerdsn.php
@@ -4,7 +4,7 @@
  * Plugin Name: Mailer DSN
  * Plugin URI:  https://github.com/PopArtDesign/wordpress-mailer-dsn
  * Description: Configure wp_mail() via environment variables (e.g. MAILER_DSN)
- * Version:     0.0.9
+ * Version:     1.0.0
  * License:     MIT
  * License URI: https://github.com/PopArtDesign/wordpress-mailer-dsn/blob/main/LICENSE
  * Author:      PopArtDesign <info@popartdesign.ru>
